2b8983f93587b1a5f02a2d4b0f307f89c750a05c
Spinelli666 <bernardospinelli@hotmail.com.br>
2024-08-30 18:40:21 -0300

    (feat): Finalização do Projeto

--- main.py
+++ main.py
@@ -17,7 +17,15 @@ def carregar_dados(empresa):
 
     return cotacoes_acao
 
-acoes = ["ITUB4.SA", "BBDC4.SA", "PETR4.SA", "VALE3.SA", "ABEV3.SA", "WEGE3.SA", "MGLU3.SA", "BBAS3.SA"]
+@st.cache_data
+def carregar_tickers_acoes():
+    base_tickers = pd.read_csv("IBOV.csv", sep=";")
+    tickers = list(base_tickers["Código"])
+    tickers = [item + ".SA" for item in tickers]
+    
+    return tickers
+
+acoes = carregar_tickers_acoes()
 dados = carregar_dados(acoes)
 # Passo 3: Criar a interface do streamlit
 
@@ -47,3 +55,50 @@ dados = dados.loc[intervalo_data[0]:intervalo_data[1]]
 
 # Criar o Gráfico
 st.line_chart(dados)
+
+
+# calculo de performance
+texto_performance_ativos = ""
+
+
+if len(lista_acoes) == 0:
+    lista_acoes = list(dados.columns)
+elif len(lista_acoes) == 1:
+    dados = dados.rename(columns = {"Close": acao_unica})
+
+carteira = [1000 for acao in lista_acoes]
+total_inicial_carteira = sum(carteira)
+
+for i, acao in enumerate(lista_acoes):
+    performance_ativo = dados[acao].iloc[-1] / dados[acao].iloc[0] - 1
+    performance_ativo = float(performance_ativo)
+
+    carteira[i] = carteira[i] * (1 + performance_ativo)
+
+    if performance_ativo > 0:
+        texto_performance_ativos = texto_performance_ativos + f"  \n{acao}: :green[{performance_ativo:.1%}]"
+    elif performance_ativo < 0:
+        texto_performance_ativos = texto_performance_ativos + f"  \n{acao}: :red[{performance_ativo:.1%}]"
+    else:
+        texto_performance_ativos = texto_performance_ativos + f"  \n{acao}: {performance_ativo:.1%}"
+
+total_final_carteira = sum(carteira)
+performance_carteira = total_final_carteira / total_inicial_carteira - 1
+
+if performance_carteira > 0:
+    texto_performance_carteira = f"Performance da carteira com todos os ativos:  :green[{performance_carteira:.1%}]"
+elif performance_ativo < 0:
+    texto_performance_carteira = f"Performance da carteira com todos os ativos:  :red[{performance_carteira:.1%}]"
+else:
+    texto_performance_carteira = f"Performance da carteira com todos os ativos:  {performance_carteira:.1%}"
+
+
+st.write(f"""
+### Performace dos Ativos
+Essa foi a performace de cada ativo no período selecionado:
+
+{texto_performance_ativos}
+
+{texto_performance_carteira}
+""")
+
